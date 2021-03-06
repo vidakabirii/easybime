862b9514b02f8545ff8e0b0f41b431d026de7059
vida kabiri <vida.kabiri.rahani@gmail.com>
2021-03-06 17:19:36 +0330

    fitst commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div id="nav">
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss" >
+@import "./assets/css/remove_default.css";
+
+@font-face {
+  font-family: "iraniansans";
+  src: url("~@/assets/fonts/iraniansans-webfont.woff") format("truetype");
+}
+
+#app {
+  font-family: "iraniansans";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <v-container>
+      <v-row>
+        <v-col style="    text-align: right;">
+          <p style="    display: inline-block;     font-size: 24px;">
+            طرح های بیمه
+          </p>
+          <img
+            style="    width: 20px;     display: inline-block;
+    align-self: center;
+    margin-left: 10px;"
+            src="../assets/icons/plan-market-icon.svg"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
